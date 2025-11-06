3b75bf12aa443a622a434f4666a1c6c61765b119
Antonio Terri95 <antonioterri95@#gmail.com>
2025-11-06 13:04:48 +0100

    nuevo

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 app.use(express.json());
 // Importar rutas
 app.use('/productos', require('./routes/productosRoutes'));
+app.use('/proveedores', require('./routes/proveedoresRoutes'));
+app.use('/carritos', require('./routes/carritosRoutes'));
 //definir el resto de routes
 //Mejora solicitada, guardar en un log de json todas las llamadas a la API
 app.listen(3000, () => console.log('Servidor escuchando en http://localhost:3000'));
