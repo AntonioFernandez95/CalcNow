8641244142b8d65407c4e4a41cb91e1aba47141f
Antonio Terri95 <antonioterri95@#gmail.com>
2025-11-06 13:16:24 +0100

    nuevo

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.use(express.json());
 app.use('/productos', require('./routes/productosRoutes'));
 app.use('/proveedores', require('./routes/proveedoresRoutes'));
 app.use('/carritos', require('./routes/carritosRoutes'));
+app.use('/pedidos', require('./routes/pedidosRoutes'));
+app.use('/categorias', require('./routes/categoriasRoutes'));
 //definir el resto de routes
 //Mejora solicitada, guardar en un log de json todas las llamadas a la API
 app.listen(3000, () => console.log('Servidor escuchando en http://localhost:3000'));
